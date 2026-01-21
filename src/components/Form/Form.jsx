8f106dd39c9f44b556ee04810fd8e2013b3d984b
Musheeb Affan <musheebaffan@Musheebs-MacBook-Air.local>
2026-01-22 00:29:43 +0530

    feat: forms in react, single sourch of truth, input > text, checkbox, radio < , textArea, etc

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,18 +2,133 @@ import "./Form.css";
 import { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("");
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    comment: "",
+    isFriendly: true,
+    areYouMad: false,
+    areYouAvailable: true,
+    areYouEvenAlive: false,
+    employment: "",
+  });
+
+  console.log(formData);
 
   function handleChange(event) {
+    const { name, value, type, checked } = event.target;
     console.log(event.target.value);
-    setFirstName((prevFirstName) => {
-      return event.target.value;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: type === "checked" ? checked : value,
+      };
     });
   }
 
   return (
     <form action="" className="form">
-      <input type="text" placeholder="First Name" onChange={handleChange} />
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={handleChange}
+        name="firstName"
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={handleChange}
+        name="lastName"
+        value={formData.lastName}
+      />
+      <input
+        type="email"
+        placeholder="enter your email"
+        onChange={handleChange}
+        name="email"
+        value={formData.email}
+      />
+      <textarea
+        placeholder="Write you comment here..."
+        onChange={handleChange}
+        name="comment"
+        value={formData.comment}
+      />
+      <div className="isFriendly-checkbox-wrapper">
+        <input
+          type="checkbox"
+          name="isFriendly"
+          id="isFriendly"
+          className="checkbox-friendly"
+          checked={formData.isFriendly}
+          onChange={handleChange}
+        />
+        <label htmlFor="isFriendly">Are you friendly?</label>
+      </div>
+      <div className="areYouMad-checkbox-wrapper">
+        <input
+          type="checkbox"
+          id="areYouMad"
+          name="areYouMad"
+          checked={formData.areYouMad}
+          onChange={handleChange}
+        />
+        <label htmlFor="areYouMad">Are you mad?</label>
+      </div>
+      <div className="areYouAvailable-checkbox-wrapper">
+        <input
+          type="checkbox"
+          id="areYouAvailable"
+          name="areYouAvailable"
+          checked={formData.areYouAvailable}
+          onChange={handleChange}
+        />
+        <label htmlFor="areYouAvailable">Are you available?</label>
+      </div>
+      <div className="areYouAlive-checkbox-wrapper">
+        <input
+          type="checkbox"
+          id="areYouEvenAlive"
+          name="areYouEvenAlive"
+          checked={formData.areYouEvenAlive}
+          onChange={handleChange}
+        />
+        <label htmlFor="areYouEvenAlive">Are you alive?</label>
+      </div>
+      <fieldset>
+        <legend>Current employment status</legend>
+        <input
+          type="radio"
+          id="unemployed-radio"
+          name="employment"
+          onChange={handleChange}
+          value="unemployed"
+          checked={formData.employment === "unemployed"}
+        />
+        <label htmlFor="unemployed-radio">Unemployed</label>
+        <br />
+        <input
+          type="radio"
+          id="part-time-radio"
+          name="employment"
+          onChange={handleChange}
+          value="part-time"
+          checked={formData.employment === "part-time"}
+        />
+        <label htmlFor="part-time-radio">Part-time</label>
+        <br />
+        <input
+          type="radio"
+          id="full-time-radio"
+          name="employment"
+          onChange={handleChange}
+          value="full-time"
+          checked={formData.employment === "full-time"}
+        />
+        <label htmlFor="full-time-radio">Full-time</label>
+      </fieldset>
     </form>
   );
 }
